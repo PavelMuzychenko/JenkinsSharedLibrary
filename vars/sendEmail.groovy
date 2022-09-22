bb666106c0d1715964b886bf2545d2fc103e450a
PavelMuzychenko <31819727+PavelMuzychenko@users.noreply.github.com>
2022-09-23 00:56:42 +0300

    Update sendEmail.groovy

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,9 +1,9 @@
 def call(String recipient){
 	node{
-	    subject: 'Build ${JOB_NAME} ${BUILD_NUMBER} has been finished ', 
-            body: 'Please go to ${BUILD_URL} and check',
-            to: recipient, 
-            attachLog: true,
-            compressLog: true
+	    emailext subject: 'Build ${JOB_NAME} ${BUILD_NUMBER} has been finished ', 
+            	body: 'Please go to ${BUILD_URL} and check',
+            	to: recipient, 
+            	attachLog: true,
+            	compressLog: true
 	}
 }
